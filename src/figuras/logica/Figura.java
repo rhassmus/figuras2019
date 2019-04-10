f8a534508d4d7391918cc3b2972e39706baa5313
rhassmus <felipeagr_@hotmail.com>
2019-04-10 10:43:12 -0500

    prueba

---
+++ src/figuras/logica/Figura.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras.logica;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public abstract class Figura {
+
+    protected Punto origen;
+    protected Punto fin;
+    protected int area;
+    protected int perimetro;
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public int getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(int perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    public Punto getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(Punto origen) {
+        this.origen = origen;
+    }
+
+    public Punto getFin() {
+        return fin;
+    }
+
+    public void setFin(Punto fin) {
+        this.fin = fin;
+    }
+
+    public abstract void calcularArea();
+
+    public abstract void calcularPerimetro();
+
+}
